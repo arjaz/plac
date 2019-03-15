1142467796043ed6e314a8b75416dbc52d7d9fb4
fluoxe7ine <locbkolocb@gmail.com>
2019-03-15 15:11:35 +0200

    Swapped translate and original word position

--- generateCSV.js
+++ generateCSV.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function writeToFile(filename, data) {
     let fileData = '';
     for (let word of data) {
-        let line = `${word.word}<br>${!word.img ? '' : '<img src="' + word.img + '">'};${!word.translate ? 'Перевод не найдено' : word.translate}<br><br>${!word.examples ? '' : word.examples}\n`;
+        let line = `${word.translate}<br>${!word.img ? '' : '<img src="' + word.img + '">'};${word.word}<br><br>${!word.examples ? '' : word.examples}\n`;
         fileData += line;
     }
     fs.writeFileSync(filename, fileData);
