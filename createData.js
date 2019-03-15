cf368c43cd74acf06ed2f761fb92a631851bf7da
fluoxe7ine <locbkolocb@gmail.com>
2019-03-15 22:19:04 +0200

    Refactor some code to 80 symbols length

--- createData.js
+++ createData.js
@@ -3,27 +3,32 @@ const $ = require('cheerio');
 
 async function createData(data) {
     let resultArr = [];
-    
+
     for (let word of data) {
-        const url = encodeURI('http://slovar.e-polish.eu/ru/slovo/' + word.split(' ').join('_'));
+        const url = encodeURI('http://slovar.e-polish.eu/ru/slovo/' +
+                    word.split(' ').join('_'));
+
         await rp(url)
             .then(html => {
                 let newWord = {};
                 const translate = $('h3.translation', html).text().trim();
-                const examples = $('p.example span.say', html).text().split('.').join('. ').trim();
+                const examples = $('p.example span.say', html).text()
+                                                              .split('.')
+                                                              .join('. ')
+                                                              .trim();
                 const img = $('img.pull-right', html).attr('src');
+
                 newWord.word = word;
                 if (examples) newWord.examples = examples;
                 if (img) newWord.img = img;
                 if (translate) {
-                    newWord.translate = translate;   
+                    newWord.translate = translate;
                     resultArr.push(newWord);
-                }; 
+                };
             })
-            .catch(err => console.log(err));    
+            .catch(err => console.log(err));
     }
-    
     return await resultArr;
 };
-    
+
 module.exports = createData;
--- generateCSV.js
+++ generateCSV.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 function writeToFile(filename, data) {
     let fileData = '';
     for (let word of data) {
-        let line = `${word.translate}<br>${!word.img ? '' : '<img src="' + word.img + '">'};${word.word}<br><br>${!word.examples ? '' : word.examples}\n`;
+        let line =  word.translate +
+                    '<br>' +
+                    (!word.img ? '' : '<img src="' + word.img + '">') +
+                    word.word +
+                    '<br><br>' +
+                    (!word.examples ? '' : word.examples) + '\n';
         fileData += line;
     }
     fs.writeFileSync(filename, fileData);
